5bebf7771753c727e9f21ddfe5db23e79b6088e9
Валерия <tima-psp2002@yandex.ru>
2024-09-22 12:04:34 +0300

    v 1.0 Рабочая

---
+++ js/OpenModal.js
@@ -0,0 +1,10 @@
+$(window).on('load',function(){
+    $('#myModal').modal('show');
+});
+
+$(document).ready(function() {
+    // Показать/скрыть блок с заголовком
+    $('.btn-secondary').click(function() {
+        $('#myModal').modal('hide');
+    });
+  });
---
+++ js/button1.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Показать/скрыть блок с заголовком
+    $('.btn-warning').click(function() {
+      $('h1').toggle();
+    });
+  });
+  
